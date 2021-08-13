ed755dff407aceb21db4feec47fb4b8c0c8d9f0c
saanvi-portfolio <88458760+saanvi-portfolio@users.noreply.github.com>
2021-08-13 14:42:51 -0700

    My first Program

---
+++ Hello_World.py
@@ -0,0 +1,15 @@
+first_name = "saanvi"
+
+      
+this_is_my_age = 11
+
+
+favorite_food = "kfc chicken burgers"
+
+
+my_favorite_movie = "harry potter and the goblet of fire"
+
+my_favorite_book = "harry potter and the goblet of fire"
+
+
+print("hi, my name is %s, and i am %i, years old, my favorite food is %s, and my favorite movie is %s, my favorite book is %s." %  (first_name, this_is_my_age, favorite_food, my_favorite_movie, my_favorite_book))
